eb5d81d53d0b4514cd09dcb8d67d3885bd0351b8
Hazem Gamal <hazemgamal223311@gmail.com>
2026-04-19 04:15:25 +0200

    change extension of image

--- api/interview_image_handler.py
+++ api/interview_image_handler.py
@@ -26,7 +26,7 @@ class InterviewImageHandler:
                 {
                     "type": "image_url",
                     "image_url": {
-                        "url": f"data:image/jpeg;base64,{image}"
+                        "url": f"data:image/png;base64,{image}"
                     },
                 },
             ]
